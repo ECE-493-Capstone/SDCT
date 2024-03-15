797a12bdb39d1ce44db2bc4824399e2a775b1ccc
Dimas Sudjito <almuhtad@ualberta.ca>
2024-03-15 15:14:26 -0600

    allow logout from manage account quick pick

--- src/ManageAccount.ts
+++ src/ManageAccount.ts
@@ -3,8 +3,10 @@ import * as vscode from 'vscode';
 export async function manageAccount() {
     const options = ["Add friend", "Create group", "See invites", "Log out"];
     const chosenOption = await vscode.window.showQuickPick(options);
-    // print the chosen option to windows
     if (!!chosenOption) {
-        vscode.window.showInformationMessage(chosenOption);
+        vscode.window.showInformationMessage(chosenOption); // DEBUG
+        if (chosenOption === "Log out") {
+            vscode.commands.executeCommand('sdct.logout');
+        }
     }
 }
