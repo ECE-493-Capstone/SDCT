60f44f4f129dd0047c0cd814401de401a43373f4
Dimas Sudjito <almuhtad@ualberta.ca>
2024-04-02 09:17:29 -0600

    update menu to add special message and show group members

--- src/services/ChatMenu.ts
+++ src/services/ChatMenu.ts
@@ -9,6 +9,7 @@ export async function chatMenu(chatRoom: IChatRoom) {
     ];
     if (chatRoom.friends.length > 1) {
         options.push("Leave Group");
+        options.push("See Group Members");
     }
     if (!chatRoom.joinedVoiceChat) {
         options.push("Join Voice Chat");
@@ -19,20 +20,57 @@ export async function chatMenu(chatRoom: IChatRoom) {
     const chosenOption = await vscode.window.showQuickPick(options);
     if (!!chosenOption) {
         if (chosenOption === "Send Media") {
-            return;
+            await sendMedia(chatRoom);
         } else if (chosenOption === "Send File") {
-            return;
+            await sendMedia(chatRoom);
         } else if (chosenOption === "Send Code Message") {
-            return;
+            await sendCodeMessage(chatRoom);
         } else if (chosenOption === "Leave Group") {
             return;
         } else if (chosenOption === "Join Voice Chat") {
             joinVoiceChat(chatRoom);
         } else if (chosenOption === "Join Code Session") {
             joinedCodeSession(chatRoom);
+        } else if (chosenOption === "See Group Members") {
+            await showGroupMembers(chatRoom);
         }
     }
 }
+
+const sendMedia = async (chatRoom: IChatRoom) => {
+    const media = await vscode.window.showOpenDialog({
+        canSelectFiles: true,
+        canSelectFolders: false,
+        canSelectMany: true,
+        filters: {
+            'Media': ['png', 'jpg', 'jpeg', 'mp4', 'gif'],
+            'All Files': ['*']
+        }
+    });
+    if (!!media) {
+        vscode.commands.executeCommand("sdct.sendMedia", chatRoom, media);
+    }
+};
+
+const sendFile = async (chatRoom: IChatRoom) => {
+    const file = await vscode.window.showOpenDialog({
+        canSelectFiles: true,
+        canSelectFolders: false,
+        canSelectMany: true,
+        filters: {
+            'All Files': ['*']
+        }
+    });
+    if (!!file) {
+        vscode.commands.executeCommand("sdct.sendFile", chatRoom, file);
+    }
+};
+
+const sendCodeMessage = async (chatRoom: IChatRoom) => {
+    const languages = ["javascript", "python", "java", "c", "c++", "c#", "typescript", "php", "ruby", "swift", "go", "rust", "kotlin", "scala", "r", "shell", "html", "css"];
+    const language = await vscode.window.showQuickPick(languages);
+    vscode.commands.executeCommand("sdct.sendCodeMessage", chatRoom, language);
+};
 
 const joinVoiceChat = (chatRoom: IChatRoom) => {
     vscode.commands.executeCommand("sdct.openVoiceChat", chatRoom);
@@ -40,4 +78,8 @@ const joinVoiceChat = (chatRoom: IChatRoom) => {
 
 const joinedCodeSession = (chatRoom: IChatRoom) => {
     vscode.commands.executeCommand("sdct.openCodeSession", chatRoom);
+};
+
+const showGroupMembers = async (chatRoom: IChatRoom) => {
+    vscode.window.showQuickPick(chatRoom.friends.map(friend => friend.name));
 };
